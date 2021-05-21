dfa97fe6ef3285688f29434eb4ad8323330d5838
sai-github <saiediitm@gmail.com>
2021-05-21 08:54:35 +0530

    fix: div obstructing clickable actions when not visible

--- src/index.js
+++ src/index.js
@@ -20,40 +20,42 @@ export const ChmodChatComponent = () => {
       <Circle className={styles.chatIcon} onClick={onToggle}>
         <ChatIcon />
       </Circle>
-      <Fade in={isOpen}>
-        <Flex
-          position='fixed'
-          bottom={24}
-          width='320px'
-          height='490px'
-          right={8}
-          rounded='md'
-          shadow='md'
-          direction='column'
-          justifyContent='space-between'
-          zIndex='2147483000'
-        >
-          <Stack direction='column' backgroundColor='teal.500' spacing={2}>
-            <Text>Your App name</Text>
-            <Text>Hi there</Text>
-            <Text>Add tagline</Text>
-          </Stack>
-          <Stack direction='column' spacing={2}>
-            <Box boxShadow='sm' p='6' rounded='md' bg='white'>
-              sm
+      {isOpen && (
+        <Fade in={isOpen}>
+          <Flex
+            position='fixed'
+            bottom={24}
+            width='320px'
+            height='490px'
+            right={8}
+            rounded='md'
+            shadow='md'
+            direction='column'
+            justifyContent='space-between'
+            zIndex='2147483000'
+          >
+            <Stack direction='column' backgroundColor='teal.500' spacing={2}>
+              <Text>Your App name</Text>
+              <Text>Hi there</Text>
+              <Text>Add tagline</Text>
+            </Stack>
+            <Stack direction='column' spacing={2}>
+              <Box boxShadow='sm' p='6' rounded='md' bg='white'>
+                sm
+              </Box>
+              <Box boxShadow='sm' p='6' rounded='md' bg='white'>
+                sm
+              </Box>
+              <Box boxShadow='sm' p='6' rounded='md' bg='white'>
+                sm
+              </Box>
+            </Stack>
+            <Box position='sticky' bottom={0}>
+              Powered by chmod777
             </Box>
-            <Box boxShadow='sm' p='6' rounded='md' bg='white'>
-              sm
-            </Box>
-            <Box boxShadow='sm' p='6' rounded='md' bg='white'>
-              sm
-            </Box>
-          </Stack>
-          <Box position='sticky' bottom={0}>
-            Powered by chmod777
-          </Box>
-        </Flex>
-      </Fade>
+          </Flex>
+        </Fade>
+      )}
     </ChakraProvider>
   )
 }
