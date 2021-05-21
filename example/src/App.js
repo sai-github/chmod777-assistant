f107ea2ec6a120f7e0b5303b097e993b5cfd5642
sai-github <saiediitm@gmail.com>
2021-05-21 18:38:46 +0530

    chore(dummy page): add dummy content to test chat component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,58 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './components/Home'
+import About from './components/About'
+import Users from './components/Users'
 
 import { ChmodChatComponent } from 'chmod777-assistant'
 import 'chmod777-assistant/dist/index.css'
 
 const App = () => {
-  return <ChmodChatComponent />
+  const chatProps = {
+    appName: 'Dummy'
+  }
+
+  return (
+    <Router>
+      <div style={{ height: '100vh', backgroundColor: 'burlywood' }}>
+        <b>Navigation:</b>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/' style={{ textDecoration: 'underline' }}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/about' style={{ textDecoration: 'underline' }}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to='/users' style={{ textDecoration: 'underline' }}>
+                Users
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <span>Current Tab"</span>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/users'>
+            <Users />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <ChmodChatComponent chatProps={chatProps} />
+      </div>
+    </Router>
+  )
 }
 
 export default App
