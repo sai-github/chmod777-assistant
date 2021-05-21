99b07d22cc1f7b8aa669cf8a156fe061ff0ef01e
sai-github <saiediitm@gmail.com>
2021-05-21 06:09:09 +0530

    style(prettier): format files with prettier

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'chmod777-assistant'
 import 'chmod777-assistant/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Example 😄' />
 }
 
 export default App
