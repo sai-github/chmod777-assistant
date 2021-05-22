ef6ac92e050f8808a61bcaf24578ca32bb998da6
sai-github <saiediitm@gmail.com>
2021-05-22 08:32:38 +0530

    feat(bugreport): implement bug reporting

--- src/components/BugReport/BugReport.js
+++ src/components/BugReport/BugReport.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+import html2canvas from 'html2canvas'
+import axios from 'axios'
+
 import {
   Box,
   Stack,
@@ -6,13 +9,59 @@ import {
   Button,
   Input,
   FormLabel,
-  Textarea
+  Textarea,
+  useToast
 } from '@chakra-ui/react'
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import styles from '../../styles.module.css'
 
 const BugReport = () => {
-  const temp = 1
+  const toast = useToast()
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+  const [submitting, setSubmitting] = useState(false)
+
+  const reportBug = () => {
+    setSubmitting(true)
+
+    const screenshotTarget = document.body
+
+    html2canvas(screenshotTarget).then((canvas) => {
+      const base64image = canvas.toDataURL('image/jpeg', 0.5)
+
+      axios
+        .post('/api/bug/savebug', {
+          title: title,
+          description: desc,
+          imageUrl: base64image,
+          priority: 'high',
+          isActiveBug: true
+        })
+        .then((res) => {
+          setSubmitting(false)
+          setTitle('')
+          setDesc('')
+          toast({
+            title: 'Success',
+            description: res.data.message,
+            status: 'success',
+            duration: 5000,
+            isClosable: true
+          })
+        })
+        .catch((e) => {
+          setSubmitting(false)
+          toast({
+            title: 'Error',
+            description: e.response.data.message,
+            status: 'error',
+            duration: 5000,
+            isClosable: true
+          })
+        })
+    })
+  }
+
   return (
     <Box
       boxShadow='md'
@@ -27,19 +76,20 @@ const BugReport = () => {
       </Text>
       <Stack spacing={4} mt={4}>
         <Box>
-          <FormLabel htmlFor='email' fontSize='sm'>
-            Email
+          <FormLabel htmlFor='title' fontSize='sm'>
+            Title
           </FormLabel>
           <Input
             size='sm'
-            id='email'
-            name='email'
+            id='title'
+            name='title'
             type='text'
-            placeholder='Enter email'
+            placeholder='Enter title'
+            onChange={(e) => setTitle(e.target.value)}
           />
           <span className={styles.errors}>
             {/* adding dummy value */}
-            {temp === 1 ? <div>Required</div> : null}
+            {!title ? <div>Required</div> : null}
           </span>
         </Box>
         <Box>
@@ -51,12 +101,13 @@ const BugReport = () => {
             id='bugDesc'
             name='bugDesc'
             type='text'
-            placeholder='Type answer'
+            placeholder='Describe your situation'
+            onChange={(e) => setDesc(e.target.value)}
           />
 
           <span className={styles.errors}>
             {/* adding dummy value */}
-            {temp === 1 ? <div>Required</div> : null}
+            {!desc ? <div>Required</div> : null}
           </span>
         </Box>
 
@@ -66,6 +117,8 @@ const BugReport = () => {
           mt={4}
           size='sm'
           colorScheme='teal'
+          onClick={reportBug}
+          isLoading={submitting}
         >
           Submit
         </Button>
